27338114f18c53a9f2f365d461cc5917deb95f8a
mishina <temma182008@gmail.com>
2022-12-29 21:35:07 +0900

    Compat for `sadd` to suppress warnings

--- test/server_helper_test.rb
+++ test/server_helper_test.rb
@@ -39,8 +39,7 @@ describe 'Resque::ServerHelper' do
 
     describe 'when the data type is set' do
       it 'returns the number of elements of the set' do
-        Resque.redis.sadd('set', 'v1')
-        Resque.redis.sadd('set', 'v2')
+        Resque.redis.sadd('set', ['v1', 'v2'])
         assert_equal 2, redis_get_size('set')
       end
     end
@@ -87,8 +86,7 @@ describe 'Resque::ServerHelper' do
 
     describe 'when the data type is set' do
       it 'returns an array of 20 elements counting from `start`' do
-        Resque.redis.sadd('set', 'v1')
-        Resque.redis.sadd('set', 'v2')
+        Resque.redis.sadd('set', ['v1', 'v2'])
         assert_equal ['v1', 'v2'], redis_get_value_as_array('set').sort
       end
     end
